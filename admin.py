268ec25bff99a9cfcb9866f398b92d442a3bdc61
Mohamed-taif <simotaif8@gmail.com>
2026-04-27 00:03:18 +0100

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import  Category, Product
 
-admin.site.register(CustomUser)
+admin.site.register(Category)
+admin.site.register(Product)
+
