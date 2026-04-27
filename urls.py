99778f09f82b7710181c7c20c056f9de5e1dd850
Mohamed-taif <simotaif8@gmail.com>
2026-04-27 16:13:02 +0100

    backend

--- urls.py
+++ urls.py
@@ -1,9 +1,33 @@
-from django.urls import path
-from . import views
-from .views import ProductListView, ProductDetailView
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),  # page d’accueil
-    path('list/', ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'), 
+    path('admin/', admin.site.urls),
+     path('', include('produit.urls')),
+    path('products/', include('produit.urls')),
+    path('users/', include('users.urls')),
+    path('orders/', include('orders.urls')),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
